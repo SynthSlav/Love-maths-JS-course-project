0c28ba5baf500fe12596efd1f6a9497a67c1ce7b
SynthSlav <ljupcebozinovski@gmail.com>
2025-03-05 10:43:14 +0100

    add code to generate 2 random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
+    /**
+     * The main game "loop", called when the script is first loaded
+     * and after the users's answer has been processed.
+     */
 
 function runGame() {
 
+    // Create 2 random numbers
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswers () {
