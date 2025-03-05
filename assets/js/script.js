dc8b1ab419a7cad5bf291fd802057780122002bc
SynthSlav <ljupcebozinovski@gmail.com>
2025-03-05 10:00:03 +0100

    added functions for the maths game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log('Connection');
+function runGame() {
+
+}
+
+function checkAnswers () {
+
+}
+
+function calculateCorrectAnswer {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
