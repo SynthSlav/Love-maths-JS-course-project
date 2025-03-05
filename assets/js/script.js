b1955111a96bed05212ee2e977a709ba035d2e0f
SynthSlav <ljupcebozinovski@gmail.com>
2025-03-05 12:42:09 +0100

    added multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,13 @@
 
         if (gameType === "addition") {
             displayAdditionQuestion(num1, num2);
+        }else if (gameType === "multiply"){
+            displayMultiplyQuestion(num1, num2);
         } else {
             alert(`Unknown game type: ${gameType}`);
             throw `Unknown game type: ${gameType}. Aborting!`;
         }
+         
     }
 
     /**
@@ -64,7 +67,9 @@
 
         if (operator === '+') {
             return  [operand1 + operand2, "addition"];
-        } else {
+        } else if (operator === 'x') {
+            return [operand1 * operand2, "multiply"];
+        }else {
             alert(`Unimplemented operator ${operator}`);
             throw(`Unimplemented operator ${operator}. Aborting`);
         }
@@ -94,6 +99,8 @@
 
     }
 
-    function displayMultiplyQuestion() {
-
+    function displayMultiplyQuestion(operand1, operand2) {
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
+        document.getElementById("operator").textContent = "x";
     }
