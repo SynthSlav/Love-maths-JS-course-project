3ac4b880e4afb4427205a70cf035127e33155cf7
SynthSlav <ljupcebozinovski@gmail.com>
2025-03-05 13:27:51 +0100

    add CI's solution for division function code compared to mine it uses multiplication

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@
         } else if (operator === '-') {
             return [operand1 - operand2, "subtract"];
         } else if (operator === '/') {
-            return [parseInt(operand1 / operand2), 'division'];
+            return [operand1 / operand2, 'division'];
         } else {
             alert(`Unimplemented operator ${operator}`);
             throw(`Unimplemented operator ${operator}. Aborting`);
@@ -125,7 +125,8 @@
     }
 
     function displayDivisionQuestion(operand1, operand2) {
-        document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-        document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+        operand1 = operand1 * operand2;
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
         document.getElementById("operator").textContent = "/";
     }
